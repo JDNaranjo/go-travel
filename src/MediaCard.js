ff108affd1e9f18323cf799002de546becb24110
JDNaranjo <juannaranjo1206@gmail.com>
2022-05-25 13:55:44 -0500

    MediaCard example updated

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 
 export default function MediaCard() {
   return (
@@ -17,17 +17,18 @@ export default function MediaCard() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          Torre inclinada de Pisa
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+        <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 1, sm: 2, md: 1 }}
+        >
+            <LocationOnIcon sx={{color: "#FF6647"}} />
+            <Typography variant="body2" color="text.secondary"  >
+                Pisa, Italia
+            </Typography>
+        </Stack>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
